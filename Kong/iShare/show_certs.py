53d4b7f2919382da4728cf2e68068595517da189
Sotirios Kakos <el21110@mail.ntua.gr>
2026-05-13 20:38:52 +0300

    Initial Structure Finished

---
+++ Kong/iShare/show_certs.py
@@ -0,0 +1,6 @@
+﻿import re
+cert_data = open('/iShare/fullchain.pem').read()
+certs = re.findall(r'-----BEGIN CERTIFICATE-----\n(.*?)\n-----END CERTIFICATE-----', cert_data, re.DOTALL)
+for i,c in enumerate(certs):
+    print('=== CERT', i, '===')
+    print(''.join(c.split()))
